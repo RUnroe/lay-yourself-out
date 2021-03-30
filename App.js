9c1d40710247ec46a0999dec37f2d740dbbea7c0
RUnroe <ryanunroe@gmail.com>
2021-03-30 14:42:50 -0600

    Add header object

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Header = () => {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+
+
 export default function App() {
   return (
     <View style={styles.container}>
