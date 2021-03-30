13906687fcf80b314125c0b82d963c0630eab53e
RUnroe <ryanunroe@gmail.com>
2021-03-30 15:40:52 -0600

    Create TextInputSection component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Picker } from 'react-native';
 
 const Header = () => {
   return (
     <View style={styles.header}>
-      <Image style={styles.logo} source={{uri: "https://steamuserimages-a.akamaihd.net/ugc/903401354517029501/A7B4CB2DCA27F6BA32486C32E86F3C1968993873/"}} />
+      <Image style={styles.logo} source={{uri: 'https://steamuserimages-a.akamaihd.net/ugc/903401354517029501/A7B4CB2DCA27F6BA32486C32E86F3C1968993873/'}} />
       <Text style={styles.headerText}>The Form</Text>
     </View>
   );
@@ -14,7 +14,23 @@ const Header = () => {
 const PersonalInfo = () => {
   return (
     <View style={styles.personalInfo}>
-      
+      <TextInputSection label='Name' placeholder='John Appleseed'/>
+      <TextInputSection label='Street Address' placeholder='123 Memory Ln'/>
+      <TextInputSection label='City' placeholder='Salt Lake City'/>
+      <TextInputSection label='State' placeholder='UT'/>
+      <TextInputSection label='Zip Code' placeholder='12345'/>
+      <Picker>
+        
+      </Picker>
+    </View>
+  );
+}
+
+const TextInputSection = (props) => {
+  return (
+    <View style={styles.inputSection}>
+      <Text>{props.label}:</Text>
+      <TextInput placeholder={props.placeholder}/>
     </View>
   );
 }
@@ -40,7 +56,7 @@ export default function App() {
   return (
     <View>
       <Header />
-
+      <PersonalInfo />
     </View>
   );
 }
@@ -56,7 +72,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#282828',
     display: 'flex',
     flexDirection: 'row',
-    alignItems: "center"
+    alignItems: 'center'
   },
   headerText: {
     fontSize: 24,
